68417a65851633b21bd4920d85204fea3443ab66
KiyonamiYu <514723273@qq.com>
2019-07-18 00:13:10 +0800

    refactor: 重新安排项目目录结构

---
+++ src/views/topic-list/index.js
@@ -0,0 +1,9 @@
+import React, { PureComponent } from 'react';
+
+export default class TopicList extends PureComponent {
+    render() {
+        return (
+            <div>Hello TopicList</div>
+        )
+    }
+}
